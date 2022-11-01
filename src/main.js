e9018a3035ead2767875d24a0406897a971450fa
Oscar Andersson <oscarandersson2@gmail.com>
2022-11-01 21:51:28 +0100

    Added scrollBehavior to main.js

--- src/main.js
+++ src/main.js
@@ -28,6 +28,17 @@ const router = createRouter({
     },
     { path: "/:notFound(.*)", component: NotFound },
   ],
+  //scrollBehavior(to, from, savedPosition){}
+  scrollBehavior(to, from, savedPosition) {
+    //console.log(to, from, savedPosition);
+
+    if (savedPosition) {
+      return savedPosition;
+    }
+
+    // This means scroll to the top of the page
+    return { left: 0, top: 0 };
+  },
 });
 
 const app = createApp(App);
